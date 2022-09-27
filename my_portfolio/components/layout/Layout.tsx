81b0226af1104d6b986c7a7665ea193f80aa599d
dawlishwarren <dawlishwarren@gmail.com>
2022-09-27 19:35:19 +0100

    planning styling and favicon added

---
+++ my_portfolio/components/layout/Layout.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { ReactNode } from 'react';
+import Footer from '../footer/Footer';
+import styles from './layout.module.scss';
+
+type Props = {
+	home?: boolean;
+	children?: ReactNode;
+	title: string;
+};
+
+const Layout = ({ home, children, title }: Props) => {
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
+			{/* If index page */}
+			{home ? (
+				<div className='container'>{children}</div>
+			) : (
+				// if inner page
+				<>
+					<nav className={styles.navbar}>Navbar</nav>
+					<div className={styles.inner_container}>{children}</div>
+				</>
+			)}
+			<Footer />
+		</>
+	);
+};
+
+export default Layout;
--- my_portfolio/components/main/Main.tsx
+++ my_portfolio/components/main/Main.tsx
@@ -1,16 +1,23 @@
-import styles from "./main.module.scss";
+import Image from 'next/image';
+import heroImg from '../../public/hero_image.png';
+import Logo from '../../assets/aw.svg';
+import styles from './main.module.scss';
 
 const Main = () => {
 	return (
-		<main className={styles.main}>
-			<div className={styles.grid}>
-				<div className={styles.title_wrapper}>
-					<h1 className={styles.title}>Alex Warren // Developer</h1>
-				</div>
-				<div className={styles.box_1}></div>
-				<div className={styles.box_2}></div>
-				<div className={styles.box_3}></div>
-				<div className={styles.box_4}></div>
+		<main>
+			<Logo />
+			<div className={styles.title_wrapper}>
+				<h1 className={styles.title}>Alex Warren // Developer</h1>
+			</div>
+			<div className={styles.image_container}>
+				<Image
+					src={heroImg}
+					alt='Picture of the author'
+					placeholder='blur'
+					layout='responsive'
+					className={`${[styles.border_circle, styles.image].join(' ')}`}
+				/>
 			</div>
 		</main>
 	);
