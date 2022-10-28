b0eef083033b7401ccbec82f8f1960462f3ac405
dawlishwarren <dawlishwarren@gmail.com>
2022-10-28 15:22:56 +0100

    navbar and modal finished

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -1,34 +1,33 @@
+// React
+import { useState } from 'react';
 // Components
-import AnimatedTitle from "../animatedTitle/AnimatedTitle";
-import SkillCategory from "./skillCategory/SkillCategory";
-import Waves from "../../assets/waves.svg";
-// Styles
-import styles from "./skills.module.scss";
-// Data
+import AnimatedTitle from '../animatedTitle/AnimatedTitle';
+import SkillCategory from './skillCategory/SkillCategory';
+import Waves from '../../assets/waves.svg';
+// Styles/Assets
+import styles from './skills.module.scss';
 import {
 	languageSkills,
 	designSkills,
 	databaseSkills,
 	testingPlanningSkills,
 	frameworkSkills,
-} from "../../data/skills";
-// React
-import { useState } from "react";
+} from '../../data/skills';
 
 const Skills = () => {
-	const [activeCategory, setActiveCategory] = useState("");
+	const [activeCategory, setActiveCategory] = useState('');
 
 	function toggleActive(category: string) {
 		setActiveCategory(category);
 	}
 
 	return (
-		<section id="skills" className={`scroll-area ${[styles.skills_section]}`}>
+		<section id='skills' className={`scroll-area ${[styles.skills_section]}`}>
 			{/* SVG overlay */}
 			<Waves className={styles.waves} />
 			{/* Section Header */}
 			<header className={styles.header}>
-				<AnimatedTitle str="My Skills" size="clamp(1rem, 6vw + 1rem, 5rem)" />
+				<AnimatedTitle str='My Skills' size='clamp(1rem, 6vw + 1rem, 5rem)' />
 			</header>
 			<div className={styles.categories_wrapper}>
 				<h4 className={styles.subtitle}>Select a category:</h4>
@@ -36,31 +35,31 @@ const Skills = () => {
 				<div className={styles.buttons_container}>
 					{/* Language */}
 					<button
-						onClick={() => toggleActive("language")}
+						onClick={() => toggleActive('language')}
 						className={styles.category_header}>
 						Language
 					</button>
 					{/* Design */}
 					<button
-						onClick={() => toggleActive("design")}
+						onClick={() => toggleActive('design')}
 						className={styles.category_header}>
 						Design
 					</button>
 					{/* Database */}
 					<button
-						onClick={() => toggleActive("database")}
+						onClick={() => toggleActive('database')}
 						className={styles.category_header}>
 						Backend Development
 					</button>
 					{/* Testing */}
 					<button
-						onClick={() => toggleActive("testing")}
+						onClick={() => toggleActive('testing')}
 						className={styles.category_header}>
 						Testing and Planning
 					</button>
 					{/* Other */}
 					<button
-						onClick={() => toggleActive("frameworks")}
+						onClick={() => toggleActive('frameworks')}
 						className={styles.category_header}>
 						Frameworks
 					</button>
@@ -69,15 +68,15 @@ const Skills = () => {
 			<>
 				{(() => {
 					switch (activeCategory) {
-						case "language":
+						case 'language':
 							return <SkillCategory skills={languageSkills} />;
-						case "design":
+						case 'design':
 							return <SkillCategory skills={designSkills} />;
-						case "database":
+						case 'database':
 							return <SkillCategory skills={databaseSkills} />;
-						case "testing":
+						case 'testing':
 							return <SkillCategory skills={testingPlanningSkills} />;
-						case "frameworks":
+						case 'frameworks':
 							return <SkillCategory skills={frameworkSkills} />;
 						default:
 							return;
--- pages/testingpage.tsx
+++ pages/testingpage.tsx
@@ -1,14 +1,16 @@
-import { NextPage } from "next";
-import Example from "../components/framerNav/Example";
-import Header from "../components/header/Header";
-import Navbar from "../components/navigation/Navbar";
+import { NextPage } from 'next';
+import AboutMe from '../components/aboutMe/AboutMe';
+import Header from '../components/header/Header';
+import Layout from '../components/layout/Layout';
+import Skills from '../components/skills/Skills';
 
 const TestingPage: NextPage = () => {
 	return (
-		<>
-			<Example />
+		<Layout title='Alex Warren | Frontend Developer' home>
 			<Header />
-		</>
+			<AboutMe />
+			<Skills />
+		</Layout>
 	);
 };
 
