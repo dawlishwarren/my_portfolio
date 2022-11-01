f31abe0df36cac0d7ef7219587a85e855ae7af29
dawlishwarren <dawlishwarren@gmail.com>
2022-11-01 00:05:09 +0000

    unescaped entities

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -32,24 +32,27 @@ const Skills = () => {
 					<div className={styles.content}>
 						<div className={styles.columns}>
 							<div className={styles.column}>
-								<h1 className={`${styles.title} ${getCategoryColor("plan")}`}>
+								<h1 className={`${styles.title} ${[getCategoryColor("plan")]}`}>
 									Planner
 								</h1>
 								<p>
 									A career accidentally built on red, green, refactor. I now
 									know the importance of planning.
 								</p>
-								<p className={`${styles.primary} ${getCategoryColor("plan")}`}>
+								<p
+									className={`${styles.primary} ${[getCategoryColor("plan")]}`}>
 									Failing to prepare:
 								</p>
 								<SkillCategory skills={plan} category={"plan"} />
-								<p className={`${styles.primary} ${getCategoryColor("plan")}`}>
+								<p
+									className={`${styles.primary} ${[getCategoryColor("plan")]}`}>
 									Preparing to fail:
 								</p>
 								<SkillCategory skills={test} category={"plan"} />
 							</div>
 							<div className={styles.column}>
-								<h1 className={`${styles.title} ${getCategoryColor("design")}`}>
+								<h1
+									className={`${styles.title} ${[getCategoryColor("design")]}`}>
 									Designer
 								</h1>
 								<p>
@@ -57,29 +60,40 @@ const Skills = () => {
 									so nice it goes unnoticed.
 								</p>
 								<p
-									className={`${styles.primary} ${getCategoryColor("design")}`}>
+									className={`${styles.primary} ${[
+										getCategoryColor("design"),
+									]}`}>
 									Business in the front:
 								</p>
 								<SkillCategory category={"design"} skills={frontEndDesign} />
 								<p
-									className={`${styles.primary} ${getCategoryColor("design")}`}>
+									className={`${styles.primary} ${[
+										getCategoryColor("design"),
+									]}`}>
 									Party in the back:
 								</p>
 								<SkillCategory category={"design"} skills={backEndDesign} />
 							</div>
 							<div className={styles.column}>
-								<h1 className={`${styles.title} ${getCategoryColor("build")}`}>
+								<h1
+									className={`${styles.title} ${[getCategoryColor("build")]}`}>
 									Builder
 								</h1>
 								<p>
 									Beguiled by the magic of code, I love building things from
 									scratch.
 								</p>
-								<p className={`${styles.primary} ${getCategoryColor("build")}`}>
+								<p
+									className={`${styles.primary} ${[
+										getCategoryColor("build"),
+									]}`}>
 									I speak:
 								</p>
 								<SkillCategory category={"build"} skills={languages} />
-								<p className={`${styles.primary} ${getCategoryColor("build")}`}>
+								<p
+									className={`${styles.primary} ${[
+										getCategoryColor("build"),
+									]}`}>
 									Library voices please:
 								</p>
 								<SkillCategory category={"build"} skills={frameworks} />
