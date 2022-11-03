6b5a4d19a4695cc8ad543ad720621641574ce581
dawlishwarren <dawlishwarren@gmail.com>
2022-11-03 16:18:37 +0000

    about finished

--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import AnimatedTitle from "../animatedTitle/AnimatedTitle";
-import styles from "./portfolio.module.scss";
+import React from 'react';
+import AnimatedTitle from '../animatedTitle/AnimatedTitle';
+import styles from './portfolio.module.scss';
 
 const Portfolio = () => {
 	return (
 		<section
-			id="portfolio"
+			id='portfolio'
 			className={`scroll-area ${[styles.portfolio_section]}`}>
-			<AnimatedTitle str="My portfolio" />
+			<div className={styles.header_wrapper}>
+				<AnimatedTitle str='My portfolio' />
+			</div>
 		</section>
 	);
 };
