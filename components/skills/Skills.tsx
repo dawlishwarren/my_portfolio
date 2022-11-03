a46e8b97c107cfcfd1cff0decbd4853264788864
dawlishwarren <dawlishwarren@gmail.com>
2022-11-03 16:19:30 +0000

    New about section made

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./skills.module.scss";
+import React from 'react';
+import styles from './skills.module.scss';
 
 import {
 	plan,
@@ -8,16 +8,16 @@ import {
 	backEndDesign,
 	languages,
 	frameworks,
-} from "../../data/skills";
-import SkillCategory from "./skillCategory/SkillCategory";
+} from '../../data/skills';
+import SkillCategory from './skillCategory/SkillCategory';
 
 const getCategoryColor = (category: string) => {
 	switch (category) {
-		case "plan":
+		case 'plan':
 			return styles.plan;
-		case "design":
+		case 'design':
 			return styles.design;
-		case "build":
+		case 'build':
 			return styles.build;
 		default:
 			break;
@@ -26,33 +26,33 @@ const getCategoryColor = (category: string) => {
 
 const Skills = () => {
 	return (
-		<section id="skills" className={`scroll-area ${[styles.skills_section]}`}>
+		<section id='skills' className={`scroll-area ${[styles.skills_section]}`}>
 			<div className={styles.container}>
 				<div className={styles.box}>
 					<div className={styles.content}>
 						<div className={styles.columns}>
 							<div className={styles.column}>
-								<h1 className={`${styles.title} ${[getCategoryColor("plan")]}`}>
+								<h1 className={`${styles.title} ${[getCategoryColor('plan')]}`}>
 									Planner
 								</h1>
 								<p>
 									A career accidentally built on red, green, refactor. I now
-									know the importance of planning.
+									understand the importance of planning.
 								</p>
 								<p
-									className={`${styles.primary} ${[getCategoryColor("plan")]}`}>
+									className={`${styles.primary} ${[getCategoryColor('plan')]}`}>
 									Failing to prepare:
 								</p>
-								<SkillCategory skills={plan} category={"plan"} />
+								<SkillCategory skills={plan} category={'plan'} />
 								<p
-									className={`${styles.primary} ${[getCategoryColor("plan")]}`}>
+									className={`${styles.primary} ${[getCategoryColor('plan')]}`}>
 									Preparing to fail:
 								</p>
-								<SkillCategory skills={test} category={"plan"} />
+								<SkillCategory skills={test} category={'plan'} />
 							</div>
 							<div className={styles.column}>
 								<h1
-									className={`${styles.title} ${[getCategoryColor("design")]}`}>
+									className={`${styles.title} ${[getCategoryColor('design')]}`}>
 									Designer
 								</h1>
 								<p>
@@ -61,22 +61,22 @@ const Skills = () => {
 								</p>
 								<p
 									className={`${styles.primary} ${[
-										getCategoryColor("design"),
+										getCategoryColor('design'),
 									]}`}>
 									Business in the front:
 								</p>
-								<SkillCategory category={"design"} skills={frontEndDesign} />
+								<SkillCategory category={'design'} skills={frontEndDesign} />
 								<p
 									className={`${styles.primary} ${[
-										getCategoryColor("design"),
+										getCategoryColor('design'),
 									]}`}>
 									Party in the back:
 								</p>
-								<SkillCategory category={"design"} skills={backEndDesign} />
+								<SkillCategory category={'design'} skills={backEndDesign} />
 							</div>
 							<div className={styles.column}>
 								<h1
-									className={`${styles.title} ${[getCategoryColor("build")]}`}>
+									className={`${styles.title} ${[getCategoryColor('build')]}`}>
 									Builder
 								</h1>
 								<p>
@@ -85,18 +85,18 @@ const Skills = () => {
 								</p>
 								<p
 									className={`${styles.primary} ${[
-										getCategoryColor("build"),
+										getCategoryColor('build'),
 									]}`}>
 									I speak:
 								</p>
-								<SkillCategory category={"build"} skills={languages} />
+								<SkillCategory category={'build'} skills={languages} />
 								<p
 									className={`${styles.primary} ${[
-										getCategoryColor("build"),
+										getCategoryColor('build'),
 									]}`}>
 									Library voices please:
 								</p>
-								<SkillCategory category={"build"} skills={frameworks} />
+								<SkillCategory category={'build'} skills={frameworks} />
 							</div>
 						</div>
 					</div>
--- data/navbarLinks.js
+++ data/navbarLinks.js
@@ -1,7 +1,8 @@
 export const links = [
-	{ index: 0, name: "About Me", href: "about" },
-	{ index: 1, name: "Portfolio", href: "portfolio" },
-	{ index: 2, name: "Skills", href: "skills" },
-	{ index: 3, name: "Contact Me", href: "contact" },
-	{ index: 4, name: "Blog", href: "blog" },
+	{ index: 0, name: 'Top', href: 'main' },
+	{ index: 1, name: 'About Me', href: 'about' },
+	{ index: 2, name: 'Skills', href: 'skills' },
+	{ index: 3, name: 'Portfolio', href: 'portfolio' },
+	{ index: 4, name: 'Contact Me', href: 'contact' },
+	{ index: 5, name: 'Blog', href: 'blog' },
 ];
