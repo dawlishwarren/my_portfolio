039346bf1db037f9c55e881fb94559bb98f3ccff
dawlishwarren <dawlishwarren@gmail.com>
2022-11-10 15:53:39 +0000

    vercel fix

--- pages/testingpage.tsx
+++ pages/testingpage.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from 'next';
 import styles from '../components/modal/modal.module.scss';
-import Header from '../components/header/Header';
 import Layout from '../components/layout/Layout';
 import { useEffect, useRef, useState } from 'react';
 
@@ -9,11 +8,7 @@ const TestingPage: NextPage = () => {
 
 	const functionVar = useRef({});
 	const ContextValue = {};
-	return (
-		<Layout title='Alex Warren | Frontend Developer' home>
-			Hello world
-		</Layout>
-	);
+	return <Layout title='Alex Warren | Frontend Developer'>Hello world</Layout>;
 };
 
 export default TestingPage;
