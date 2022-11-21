40b75e5f9c3ef1bf7371a55d071cfb65e1884e7a
dawlishwarren <dawlishwarren@gmail.com>
2022-11-21 17:23:56 +0000

    hover implemented and moved to own module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,33 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from 'next-themes';
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider enableSystem={true}>
 			<Head>
-				<link rel='icon' href='my_favicon.ico' />
+				<link rel="icon" href="my_favicon.ico" />
 				<link
-					rel='apple-touch-icon'
-					sizes='180x180'
-					href='/apple-touch-icon.png'
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
 				/>
 				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='/favicon-32x32.png'
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
 				/>
 				<link
-					rel='icon'
-					type='image/png'
-					sizes='16x16'
-					href='/favicon-16x16.png'
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
 				/>
-				<link rel='manifest' href='/site.webmanifest' />
-				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
-				<meta name='msapplication-TileColor' content='#da532c' />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#da532c" />
 			</Head>
 
 			<Component {...pageProps} />
