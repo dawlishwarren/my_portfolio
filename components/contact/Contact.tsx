462563b5d3f3a66f8d706c74989f93f884b90d80
dawlishwarren <dawlishwarren@gmail.com>
2022-12-16 16:34:03 +0000

    made background into a component

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,33 +1,35 @@
 // React/Next/NPM
-import React from "react";
+import React from 'react';
 // Components
-import AnimatedTitle from "../animatedTitle/AnimatedTitle";
-import { CgAttachment } from "react-icons/cg";
-import { GoMail } from "react-icons/go";
-import { SlSocialLinkedin } from "react-icons/sl";
+import AnimatedTitle from '../animatedTitle/AnimatedTitle';
+import { CgAttachment } from 'react-icons/cg';
+import { GoMail } from 'react-icons/go';
+import { SlSocialLinkedin } from 'react-icons/sl';
 // Styles/Assets
-import styles from "./contact.module.scss";
+import styles from './contact.module.scss';
 
 const Contact = () => {
 	return (
-		<section id="contact" className={`scroll-area ${[styles.contact_section]}`}>
-			<div className={styles.box}>
-				<div className={styles.header_wrapper}>
-					<AnimatedTitle str="Get in touch" />
-				</div>
-				<div className={styles.content}>
-					<div className={styles.columns}>
-						<div className={styles.column}>
-							<CgAttachment size={32} />
-							<h1 className={styles.title}>Read my Résumé</h1>
-						</div>
-						<div className={styles.column}>
-							<GoMail size={32} />
-							<h1 className={styles.title}>Send me an Email</h1>
-						</div>
-						<div className={styles.column}>
-							<SlSocialLinkedin size={32} />
-							<h1 className={styles.title}>Find me on LinkedIn</h1>
+		<section id='contact' className={`scroll-area ${[styles.contact_section]}`}>
+			<div className={styles.container}>
+				<div className={styles.box}>
+					<div className={styles.header_wrapper}>
+						<AnimatedTitle str='Get in touch' />
+					</div>
+					<div className={styles.content}>
+						<div className={styles.columns}>
+							<div className={styles.column}>
+								<CgAttachment size={32} />
+								<h1 className={styles.title}>Read my Résumé</h1>
+							</div>
+							<div className={styles.column}>
+								<GoMail size={32} />
+								<h1 className={styles.title}>Send me an Email</h1>
+							</div>
+							<div className={styles.column}>
+								<SlSocialLinkedin size={32} />
+								<h1 className={styles.title}>Find me on LinkedIn</h1>
+							</div>
 						</div>
 					</div>
 				</div>
