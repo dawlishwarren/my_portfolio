73ba349d668e24485333a03925411976abec746f
dawlishwarren <dawlishwarren@gmail.com>
2023-02-09 17:04:55 +0000

    Update to portfolio section

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,38 +1,36 @@
 // React/Next/NPM
-import React from 'react';
+import React from "react";
 // Components
-import AnimatedTitle from '../animatedTitle/AnimatedTitle';
-import { CgAttachment } from 'react-icons/cg';
-import { GoMail } from 'react-icons/go';
-import { SlSocialLinkedin } from 'react-icons/sl';
+import AnimatedTitle from "../animatedTitle/AnimatedTitle";
+import { CgAttachment } from "react-icons/cg";
+import { GoMail } from "react-icons/go";
+import { SlSocialLinkedin } from "react-icons/sl";
 // Styles/Assets
-import styles from './contact.module.scss';
-import Link from 'next/link';
+import styles from "./contact.module.scss";
+import Link from "next/link";
 
 const Contact = () => {
 	return (
-		<section id='contact' className={`scroll-area ${[styles.contact_section]}`}>
+		<section id="contact" className={`scroll-area ${[styles.contact_section]}`}>
 			<div className={styles.container}>
 				<div className={styles.box}>
 					<div className={styles.header_wrapper}>
-						<AnimatedTitle str='Get in touch' />
+						<AnimatedTitle str="Get in touch" />
 					</div>
 					<div className={styles.content}>
 						<div className={styles.columns}>
 							<div className={styles.column}>
-								<CgAttachment size={32} />
+								<CgAttachment className={styles.icon} size={32} />
 								<h1 className={styles.title}>Read my Résumé</h1>
 							</div>
+							<Link href="mailto:alexwarrendev@gmail.com">
+								<div className={styles.column}>
+									<GoMail className={styles.icon} size={32} />
+									<h1 className={styles.title}>Send me an Email</h1>
+								</div>
+							</Link>
 							<div className={styles.column}>
-								<GoMail size={32} />
-								<h1 className={styles.title}>
-									<Link href='mailto:dawlishwarren@gmail.com'>
-										Send me an Email
-									</Link>
-								</h1>
-							</div>
-							<div className={styles.column}>
-								<SlSocialLinkedin size={32} />
+								<SlSocialLinkedin className={styles.icon} size={32} />
 								<h1 className={styles.title}>Find me on LinkedIn</h1>
 							</div>
 						</div>
--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -1,19 +1,19 @@
 // React/Next/NPM
-import React, { useRef, useState } from 'react';
-import Image, { StaticImageData } from 'next/image';
-import { AnimatePresence, m, useScroll } from 'framer-motion';
+import React, { useRef, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import { AnimatePresence, m, useScroll } from "framer-motion";
 // Components
-import AnimatedTitle from '../animatedTitle/AnimatedTitle';
-import Modal from '../modal/Modal';
+import AnimatedTitle from "../animatedTitle/AnimatedTitle";
+import Modal from "../modal/Modal";
 // Styles/Assets
-import styles from './portfolio.module.scss';
-import tourism from '../../public/tourism.png';
-import directory from '../../public/directory.png';
-import logo from '../../public/logo.png';
-import portfolio from '../../public/portfolio.png';
-import consultation from '../../public/consultation.png';
-import { AiFillGithub } from 'react-icons/ai';
-import ProgressEllipse from '../progressEllipse/ProgressEllipse';
+import styles from "./portfolio.module.scss";
+import tourism from "../../public/tourism.png";
+import directory from "../../public/directory.png";
+import logo from "../../public/logo.png";
+import portfolio from "../../public/portfolio.png";
+import consultation from "../../public/consultation.png";
+import { AiFillGithub } from "react-icons/ai";
+import ProgressEllipse from "../progressEllipse/ProgressEllipse";
 // Prop Types
 interface Carousel {
 	title: string;
@@ -28,42 +28,42 @@ interface Carousel {
 const carouselData = [
 	{
 		index: 0,
-		title: 'Vanilla HTML',
+		title: "Vanilla HTML",
 		url: tourism,
-		alt: 'HTML Tourism website homepage screenshot',
+		alt: "HTML Tourism website homepage screenshot",
 		text: (
 			<p className={styles.text}>
-				A tourism website built for my hometown created using{' '}
-				<span className={styles.strong}>vanilla HTML, CSS and JavaScript</span>.{' '}
+				A tourism website built for my hometown created using{" "}
+				<span className={styles.strong}>vanilla HTML, CSS and JavaScript</span>.{" "}
 				This was my first project after completing Udemy courses.
 			</p>
 		),
-		repo: 'https://github.com/dawlishwarren/visit_budleigh',
+		repo: "https://github.com/dawlishwarren/visit_budleigh",
 	},
 	{
 		index: 1,
-		title: 'Custom UI',
+		title: "Custom UI",
 		url: directory,
-		alt: 'Business directory UI',
+		alt: "Business directory UI",
 		text: (
 			<p className={styles.text}>
 				This is a full-stack Next.js business directory app. Built with MongoDB,
-				AWS S3 and Next.js, it showcases a{' '}
+				AWS S3 and Next.js, it showcases a{" "}
 				<span className={styles.strong}>fully custom UI.</span>
 			</p>
 		),
-		repo: 'https://github.com/dawlishwarren/BSCC',
+		repo: "https://github.com/dawlishwarren/BSCC",
 	},
 	{
 		index: 2,
-		title: 'SVG Design',
+		title: "SVG Design",
 		url: logo,
-		alt: 'Website logo',
+		alt: "Website logo",
 		text: (
 			<p className={styles.text}>
 				Perhaps you&apos;ve already spotted the Favicon in your browser tab.
 				Inspired by the unofficial JavaScript logo designed by Chris Williams,
-				the logo for my site was created in Figma and one of the{' '}
+				the logo for my site was created in Figma and one of the{" "}
 				<span className={styles.strong}>many uses of SVGs</span> dotted
 				throughout my site.
 			</p>
@@ -71,36 +71,36 @@ const carouselData = [
 	},
 	{
 		index: 3,
-		title: 'Responsive Layout',
+		title: "Responsive Layout",
 		url: portfolio,
-		alt: 'Landing page with responsive layout',
+		alt: "Landing page with responsive layout",
 		text: (
 			<p className={styles.text}>
 				My first draft of a portfolio website. Heavily inspired by the art
 				language of Piet Mondrian and of layouts that make use of the principles
-				of the Golden Ratio. While only a sketch it is a good example of a{' '}
+				of the Golden Ratio. While only a sketch it is a good example of a{" "}
 				<span className={styles.strong}>fully responsive</span> design.
 			</p>
 		),
 	},
 	{
 		index: 4,
-		title: 'Consultation',
+		title: "Consultation",
 		url: consultation,
-		alt: 'Screenshot of tourism website',
+		alt: "Screenshot of tourism website",
 		text: (
 			<p className={styles.text}>
 				The working version of the hometown tourism website featured on the
 				first slide, I was heavily involved in the creation of this site. It
-				involved <span className={styles.strong}>working with the client</span>{' '}
+				involved <span className={styles.strong}>working with the client</span>{" "}
 				to better identify what the brief was, and how to overcome issues
-				related to{' '}
+				related to{" "}
 				<span className={styles.strong}>
 					SEO, authentication requirements, and UI design choices.
 				</span>
 			</p>
 		),
-		link: 'https://www.lovebudleigh.co.uk/',
+		link: "https://www.lovebudleigh.co.uk/",
 	},
 ];
 
@@ -108,13 +108,13 @@ const Portfolio = () => {
 	// Modal State
 	const [modalOpen, setModalOpen] = useState(false);
 	const [activePanel, setActivePanel] = useState<Carousel>({
-		title: '',
-		url: { src: '', height: 0, width: 0 },
+		title: "",
+		url: { src: "", height: 0, width: 0 },
 		index: 0,
-		alt: '',
+		alt: "",
 		text: <></>,
-		repo: '',
-		link: '',
+		repo: "",
+		link: "",
 	});
 	const close = () => setModalOpen(false);
 
@@ -123,20 +123,18 @@ const Portfolio = () => {
 
 	return (
 		<section
-			id='portfolio'
+			id="portfolio"
 			className={`scroll-area ${[styles.portfolio_section]}`}>
 			{/* HEADER WITH PROGRESS SVG */}
 			<div className={styles.header_wrapper}>
-				<AnimatedTitle str='My P' />
+				<AnimatedTitle str="My P" />
 				<ProgressEllipse progress={scrollXProgress} />
-				<AnimatedTitle str='rtfolio' />
+				<AnimatedTitle str="rtfolio" />
 			</div>
 			<div className={styles.carousel_container}>
 				<div className={styles.carousel_header}>
-					<h2 className={styles.carousel_header_main}>Check out my work, </h2>
-					<span className={styles.carousel_header_secondary}>
-						see my progression.
-					</span>
+					<h2 className={styles.carousel_header_main}>Take a scroll, </h2>
+					<span className={styles.carousel_header_secondary}>see my work.</span>
 				</div>
 				<ul className={styles.carousel} ref={ref}>
 					{carouselData.map((slide, index) => (
@@ -151,17 +149,17 @@ const Portfolio = () => {
 							<div className={styles.slide_image}>
 								<Image
 									src={slide.url}
-									width={650}
-									height={300}
-									objectFit='scale-down'
+									width={700}
+									height={350}
+									objectFit="scale-down"
 									alt={slide.alt}
-									placeholder='empty'
+									placeholder="empty"
 								/>
 							</div>
 						</m.li>
 					))}
 				</ul>
-				<AnimatePresence initial={false} mode='wait'>
+				<AnimatePresence initial={false} mode="wait">
 					{modalOpen && (
 						<Modal modalOpen={modalOpen} handleClose={close}>
 							<div className={styles.modal_wrapper}>
@@ -174,9 +172,9 @@ const Portfolio = () => {
 											src={activePanel.url}
 											width={600}
 											height={500}
-											objectFit={'scale-down'}
+											objectFit={"scale-down"}
 											alt={activePanel.alt}
-											placeholder='empty'
+											placeholder="empty"
 										/>
 									</div>
 									<div className={styles.text_wrapper}>
@@ -186,8 +184,8 @@ const Portfolio = () => {
 										{activePanel.repo && (
 											<a
 												href={activePanel.repo}
-												target='_blank'
-												rel='noreferrer'>
+												target="_blank"
+												rel="noreferrer">
 												<button className={styles.modal_button}>
 													Check out the repo
 													<AiFillGithub size={20} />
@@ -197,8 +195,8 @@ const Portfolio = () => {
 										{activePanel.link && (
 											<a
 												href={activePanel.link}
-												target='_blank'
-												rel='noreferrer'>
+												target="_blank"
+												rel="noreferrer">
 												<button className={styles.modal_button}>
 													See the site
 												</button>
