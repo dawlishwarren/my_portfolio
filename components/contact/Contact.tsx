40d8f60665aa4a696825011e055f73b096ca6f4d
dawlishwarren <dawlishwarren@gmail.com>
2023-02-23 21:37:03 +0000

    CV added and hover effect on contact

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,39 +1,54 @@
 // React/Next/NPM
-import React from "react";
+import React from 'react';
 // Components
-import AnimatedTitle from "../animatedTitle/AnimatedTitle";
-import { CgAttachment } from "react-icons/cg";
-import { GoMail } from "react-icons/go";
-import { SlSocialLinkedin } from "react-icons/sl";
+import AnimatedTitle from '../animatedTitle/AnimatedTitle';
+import { CgAttachment } from 'react-icons/cg';
+import { GoMail } from 'react-icons/go';
+import { SlSocialLinkedin } from 'react-icons/sl';
 // Styles/Assets
-import styles from "./contact.module.scss";
-import Link from "next/link";
+import styles from './contact.module.scss';
+import hoverStyles from '../../styles/hover.module.scss';
+import Link from 'next/link';
 
 const Contact = () => {
+	const handleMouseMove = (e: React.MouseEvent<HTMLUListElement>) => {
+		e.preventDefault();
+		for (const item of document.getElementsByClassName(
+			`${hoverStyles.contact_item}`
+		) as HTMLCollectionOf<HTMLLIElement>) {
+			const rect = item.getBoundingClientRect(),
+				x = e.clientX - rect.left,
+				y = e.clientY - rect.top;
+			item.style.setProperty('--mouse-x', `${x}px`);
+			item.style.setProperty('--mouse-y', `${y}px`);
+		}
+	};
 	return (
-		<section id="contact" className={`scroll-area ${[styles.contact_section]}`}>
+		<section id='contact' className={`scroll-area ${[styles.contact_section]}`}>
 			<div className={styles.container}>
 				<div className={styles.box}>
 					<div className={styles.header_wrapper}>
-						<AnimatedTitle str="Get in touch" />
+						<AnimatedTitle str='Get in touch' />
 					</div>
 					<div className={styles.content}>
-						<div className={styles.columns}>
-							<div className={styles.column}>
-								<CgAttachment className={styles.icon} size={32} />
-								<h1 className={styles.title}>Read my Résumé</h1>
-							</div>
-							<Link href="mailto:alexwarrendev@gmail.com">
-								<div className={styles.column}>
+						<ul className={styles.columns} onMouseMove={handleMouseMove}>
+							<Link href='https://docs.google.com/document/d/1Omfi4afRCgX-qENdIJm3RRpqHIJ-yTgIK0bCb7KBPy0/edit?usp=sharing'>
+								<li className={`${styles.column} ${hoverStyles.contact_item}`}>
+									<CgAttachment className={styles.icon} size={32} />
+									<h1 className={styles.title}>Read my Résumé</h1>
+								</li>
+							</Link>
+							<Link href='mailto:alexwarrendev@gmail.com'>
+								<li className={`${styles.column} ${hoverStyles.contact_item}`}>
 									<GoMail className={styles.icon} size={32} />
 									<h1 className={styles.title}>Send me an Email</h1>
-								</div>
+								</li>
 							</Link>
-							<div className={styles.column}>
+							<li className={`${styles.column} ${hoverStyles.contact_item}`}>
 								<SlSocialLinkedin className={styles.icon} size={32} />
 								<h1 className={styles.title}>Find me on LinkedIn</h1>
-							</div>
-						</div>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
